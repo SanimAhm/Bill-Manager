5e6178644d56799837ce54138f95d7089857369c
SanimAhm <sanimahm77@gmail.com>
2025-12-03 12:17:37 +0600

    update

--- Bill/admin.py
+++ Bill/admin.py
@@ -3,7 +3,9 @@ from .models import Bill
 
 # Register your models here.
 class BillAdmin(admin.ModelAdmin):
-    list_display = ['supplier_name', 'bill_value']
+    list_display = ['supplier_name', 'bill_value', 'net_payment', 'status']
+    search_fields = ['supplier_name']
+    list_filter = ['status', 'receive_date']
     
 admin.site.register(Bill, BillAdmin)
 
